66dcd6d5fc833156937548790006016a95cbd1be
daniel.carvalho <daniel.carvalho@maxmilhas.com.br>
2022-10-10 21:11:59 -0300

    Complete feed styling

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/DanielM08.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Daniel Carvalho</strong>
+              <time title= "09 de outubro às 16" dateTime="2022-10-09 16:00:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
